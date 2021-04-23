b4ba0b029d88adc2e783085c6f25af62a1902ddb
gunwoongPark <olympiodoros@naver.com>
2021-04-23 18:44:24 +0900

    10번 시간초과, 12번 런타임 에러

---
+++ 프로그래머스/큰 수 만들기.py
@@ -0,0 +1,24 @@
+def solution(number, k):
+    stack = []
+    num_list = list(number)
+    stack.append(num_list.pop(0))
+
+    while True:
+        if k == 0:
+            break
+
+        if len(stack) == 0:
+            stack.append(num_list.pop(0))
+
+        elif num_list[0] > stack[-1]:
+            stack.pop(-1)
+            k -= 1
+        elif num_list[0] <= stack[-1]:
+            stack.append(num_list.pop(0))
+
+
+    answer = "".join(stack + num_list)
+
+    return answer
+
+print(solution("1231234", 3))
