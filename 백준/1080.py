12a82eb6edf934cfb82a691e5a80e3ff60509a07
gunwoongPark <olympiodoros@naver.com>
2021-04-27 00:47:55 +0900

    no message

--- 백준/1080.py
+++ 백준/1080.py
@@ -1,10 +1,10 @@
-def convertMat(idx, jdx, Aarr, Atarget):
-    for target_idx, origin_idx in enumerate(range(idx, idx+3)):
-        for target_jdx, origin_jdx in enumerate(range(jdx, jdx+3)):
-            if Atarget[target_idx][target_jdx] == 1:
-                Aarr[origin_idx][origin_jdx] = 0
+def convertMat(idx, jdx, Aarr):
+    for i in range(idx, idx + 3):
+        for j in range(jdx, jdx + 3):
+            if bool(Aarr[i][j]):
+                Aarr[i][j] = 0
             else:
-                Aarr[origin_idx][origin_jdx] = 1
+                Aarr[i][j] = 1
 
 if __name__ == '__main__':
     N, M = map(int, input().split())
@@ -20,17 +20,13 @@ if __name__ == '__main__':
 
     count = 0
 
-    for idx in range(N - 2):
-        for jdx in range(M - 2):
-            Atarget = [row[jdx:jdx+3] for row in Aarr[idx:idx+3]]
-            Btarget = [row[jdx:jdx+3] for row in Barr[idx:idx+3]]
-            if Atarget == Btarget:
-                continue
-            else:
-                convertMat(idx, jdx, Aarr, Atarget)
+    for idx in range(N-2):
+        for jdx in range(M-2):
+            if Aarr[idx][jdx] != Barr[idx][jdx]:
                 count += 1
+                convertMat(idx, jdx, Aarr)
 
-    if Aarr == Barr:
-        print(count)
-    else:
+    if Aarr != Barr:
         print(-1)
+    else:
+        print(count)
