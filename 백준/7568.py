ecef98390e9de869ce6c71ef71d17231ea87d579
gunwoongPark <olympiodoros@naver.com>
2021-05-22 20:42:34 +0900

    no message

--- 백준/7568.py
+++ 백준/7568.py
@@ -4,3 +4,14 @@ people = []
 for _ in range(N):
     people.append(tuple(map(int, input().split())))
 
+result = []
+
+for person in people:
+    rank = 1
+    for idx in range(len(people)):
+        if person[0] < people[idx][0] and person[1] < people[idx][1]:
+            rank += 1
+    result.append(rank)
+
+for el in result:
+    print(el, end=" ")
