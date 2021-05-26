f73a84bb52eed60942630c2a7fad6dc47b96384c
gunwoongPark <olympiodoros@naver.com>
2021-05-26 14:49:45 +0900

    no message

--- 백준/11724.py
+++ 백준/11724.py
@@ -1,1 +1,37 @@
-N, M = map(int, input().split())
+from sys import setrecursionlimit
+from sys import stdin
+setrecursionlimit(10**9)
+
+def DFS(graph, V, visited):
+    if visited[V] == True:
+        return
+
+    visited[V] = True
+
+    if len(graph[V]) == 0:
+        return True
+
+    for i in graph[V]:
+        if not visited[i]:
+            DFS(graph, i, visited)
+            return True
+
+if __name__ == '__main__':
+    N, M = map(int, stdin.readline().split())
+
+    graph = [[] for _ in range(N+1)]
+
+    for _ in range(M):
+        v1, v2 = map(int, stdin.readline().split())
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+
+    visited = [False] * (N+1)
+
+    count = 0
+
+    for idx in range(1, N+1):
+        if DFS(graph, idx, visited):
+            count += 1
+
+    print(count)
